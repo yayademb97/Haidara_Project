c5bfde6d49699889c6a8377df47f8faf018bc08f
yayademb97 <yayadembele09@gmail.com>
2025-06-24 21:07:42 +0100

    New commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+
+function App() {
+
+  return (
+    <>
+      <Footer />
+      <Hero />
+    </>
+  );
+}
+
+export default App;
